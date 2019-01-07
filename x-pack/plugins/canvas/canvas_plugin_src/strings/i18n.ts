5eff33e9c5842ed5d301f7f0abf1652adfdc94b6
Clint Andrew Hall <clint.hall@elastic.co>
2019-01-07 16:45:21 -0600

    Fixing Kibana test:server call

--- x-pack/plugins/canvas/canvas_plugin_src/strings/i18n.ts
+++ x-pack/plugins/canvas/canvas_plugin_src/strings/i18n.ts
@@ -4,4 +4,9 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export const i18n = typeof STUB_CANVAS_I18N !== 'undefined' ? STUB_CANVAS_I18N : canvas.i18n;
+export const i18n =
+  typeof STUB_CANVAS_I18N !== 'undefined'
+    ? STUB_CANVAS_I18N
+    : typeof canvas !== 'undefined'
+    ? canvas.i18n
+    : {};
